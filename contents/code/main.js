6b82a89d6e2c8ddc62e9b77d8fdda90c8d094aa7
siimpliidiip <48057244+siimpliidiip@users.noreply.github.com>
2022-07-18 23:11:08 +0530

    Add Code for Bottom Placement
    Bottom Placement Function Added & Registered a sample shortcut.
    Fixed bug, vertical ratio not followed.

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,4 +1,4 @@
-function setCurrentWindowSize(horizontalRatio, verticalRatio) {
+function setCurrentWindowCenter(horizontalRatio, verticalRatio) {
 
     var currentWindow = workspace.activeClient;
 
@@ -6,7 +6,7 @@ function setCurrentWindowSize(horizontalRatio, verticalRatio) {
         var currentDisplay = workspace.clientArea(KWin.MaximizeArea, currentWindow);
 
         var windoWidth = currentDisplay.width * horizontalRatio;
-        var windowHeight = currentDisplay.height * horizontalRatio;
+        var windowHeight = currentDisplay.height * verticalRatio;
 
         var dx = (currentDisplay.width - windoWidth) / 2;
         var dy = (currentDisplay.height - windowHeight) / 2;
@@ -20,44 +20,75 @@ function setCurrentWindowSize(horizontalRatio, verticalRatio) {
         };
     }
 }
+
+function setCurrentWindowBottom(horizontalRatio, verticalRatio) {
+
+    var currentWindow = workspace.activeClient;
+
+    if (currentWindow.moveable) {
+        var currentDisplay = workspace.clientArea(KWin.MaximizeArea, currentWindow);
+
+        var windoWidth = currentDisplay.width * horizontalRatio;
+        var windowHeight = currentDisplay.height * verticalRatio;
+
+        var dx = (currentDisplay.width - windoWidth) / 2;
+        var dy = (currentDisplay.height - windowHeight) / 2 * verticalRatio;
+
+        var X = currentDisplay.x + dx;
+        var Y = currentDisplay.y + currentDisplay.height - windowHeight - dy;
+
+        currentWindow.geometry = {
+            x: X, y: Y,
+            width: windoWidth, height: windowHeight
+        };
+    }
+}
+
 registerShortcut(
-    "Reduce Windows to 75% Width Height and Center (HD)",
+    "Reduce Windows to 75% Width Height and Center (720)",
     "Center & 3/4th-ify Window Size to the Monitor",
     "Ctrl+Meta+Z",
-    function () {setCurrentWindowSize(.75,.75);}
+    function () {setCurrentWindowCenter(.75,.75);}
     )
 
 registerShortcut(
-    "Reduce Windows to 66% Width Height and Center (HD)",
+    "Reduce Windows to 66% Width Height and Center (1080)",
     "Center & 2/3rd-ify Window Size to the Monitor",
     "Ctrl+Meta+Z",
-    function () {setCurrentWindowSize(.66,.66);}
+    function () {setCurrentWindowCenter(.66,.66);}
     )
 
 
 registerShortcut(
-    "Reduce Windows to 50% Width Height and Center (1440p)",
+    "Reduce Windows to 50% Width Height and Center (1440)",
     "Center & Half-ify Window Size to the Monitor and Center",
     "Ctrl+Meta+Z",
-    function () {setCurrentWindowSize(.5,.5);}
+    function () {setCurrentWindowCenter(.5,.5);}
     )
 
 registerShortcut(
     "Reduce Windows to 33% Width Height and Center (4K)",
     "Center & Third-ify Window Size to the Monitor",
     "Ctrl+Meta+Z",
-    function () {setCurrentWindowSize(.33,.33);}
+    function () {setCurrentWindowCenter(.33,.33);}
     )
 
 registerShortcut(
-    "Reduce Windows to 25% Width Height and Center (HD)",
+    "Reduce Windows to 25% Width Height and Center (8K)",
     "Center & Quarter-ify Window Size to the Monitor",
     "Ctrl+Meta+Z",
-    function () {setCurrentWindowSize(.25,.25);}
+    function () {setCurrentWindowCenter(.25,.25);}
+    )
+
+registerShortcut(
+    "Reduce Windows to 75% Width 25% Height and Bottom (1440)",
+    "Bottom & Quarter-ify Window Size to the Monitor",
+    "Ctrl+Meta+X",
+    function () {setCurrentWindowBottom(.75,.25);}
     )
 
 //registerShortcut(
-//    "If you are modding Place ->Description of Function here <-",
+//    "If you are modding, Place ->Description of Function here <-",
 //    "->This Text will go to the Shortcuts List<-",
 //    "->Ctrl+Meta+Z<-",  //The default Keyboard Shortcut for this function
 //    function () {setCurrentWindowSize(.75,.75);}  //call the function with your custom values
